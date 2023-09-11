c574ebed4916db1179207f602afad6593b9b276c
migueldmo <89785768+migueldmo@users.noreply.github.com>
2023-09-11 15:29:09 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,50 +1,71 @@
 import { Bike } from "./bike";
-import { Rent } from "./rent";
+import { Rent } from "./rents";
 import { User } from "./user";
-
+import crypto from 'crypto'
 
 export class App {
     users: User[] = []
     bikes: Bike[] = []
     rents: Rent[] = []
 
-    findUser(email: string): User | undefined {
+    findUser(email: string): User {
         return this.users.find(user => user.email === email)
     }
 
-    removeUser(email: string): void{
-        const index = this.users.findIndex(user => user.email === email)
-        const hUsers = this.users.slice(0, index)
-        const xUsers = this.users[index+1]
-        const zUsers = hUsers.concat(xUsers)
-    }
-
-    returnBike(bike: Bike): void{
-
-    }
-    rentBike(rents: Rent[], bike: Bike, user: User, 
-        startDate: Date, endDate: Date): void{
-        Rent.create(rents, bike, user, startDate, endDate)
-        
-    }
-
-    registerBike(bike: Bike): void{
-        for(const rBike of this.bikes){
-            if(rBike.id === bike.id){
-                throw new Error('Duplicated bike!')
-            }
-        }
-        bike.id = crypto.randomUUID()
-        this.bikes.push(bike)
-    }
-
-    registerUser(user: User): void {
+    registerUser(user: User): string {
         for (const rUser of this.users) {
-            if (rUser.email === user.email){
+            if (rUser.email === user.email) {
                 throw new Error('Duplicate user.')
             }
         }
-        user.id = crypto.randomUUID()
+        const newId = crypto.randomUUID()
+        user.id = newId
         this.users.push(user)
+        return newId
+    }
+
+    registerBike(bike: Bike): string {
+        const newId = crypto.randomUUID()
+        bike.id = newId
+        this.bikes.push(bike)
+        return newId
+    }
+
+    removeUser(email: string): void {
+        const userIndex = this.users.findIndex(user => user.email === email)
+        if (userIndex !== -1) {
+            this.users.splice(userIndex, 1)
+            return
+        }
+        throw new Error('User does not exist.')
+    }
+    
+    rentBike(bikeId: string, userEmail: string, start: Date): void {
+        const bike = this.bikes.find(bike => bike.id === bikeId)
+        if (!bike) {
+            throw new Error('Bike not found.')
+        }
+        const user = this.findUser(userEmail)
+        if (!user) {
+            throw new Error('User not found.')
+        }
+        if(!bike.available){
+            throw new Error('Bike Unavailable')
+        }
+        const newRent = Rent.create(this.bikes, bike, user, start)
+
+        this.rents.push(newRent)
+    }
+
+    returnBike(bikeId: string, userEmail: string, start: Date): number {
+        const end = new Date
+        const rent = this.rents.find(rent => 
+            rent.bike.id === bikeId 
+        )
+        if (rent) {
+            const diff = Math.abs(end - start)
+            return diff * rent.bike.rate
+        }
+        throw new Error('Rent not found.')
     }
 }
