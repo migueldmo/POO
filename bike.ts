c22383075e9a92a6af7d98a9777f0c58d0746895
migueldmo <89785768+migueldmo@users.noreply.github.com>
2023-09-18 10:32:13 -0300

    Create bike.ts

---
+++ bike.ts
@@ -0,0 +1,16 @@
+export class Bike {
+    constructor(
+        public name: string,
+        public type: string,
+        public bodySize: number,
+        public maxLoad: number,
+        public rate: number,
+        public description: string,
+        public ratings: number,
+        public imageUrls: string[],
+        public available: boolean = true,
+        public lat: number, //Latitude
+        public long: number, //Longitude
+        public id?: string,
+    ) {}
+}
