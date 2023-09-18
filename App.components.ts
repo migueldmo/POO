5fdd17e4b8acf350c2ce23ea9d55c1876d05fcbf
migueldmo <89785768+migueldmo@users.noreply.github.com>
2023-09-18 10:31:45 -0300

    Create App.components.ts

---
+++ App.components.ts
@@ -0,0 +1,9 @@
+import { Bike } from './bike';
+
+import { Component } from '@angular/core';
+
+export class AppComponent{
+    title = 'angular-location';
+    this.bike.lat: number = 0;
+    this.bike.long: number = 0;
+}
